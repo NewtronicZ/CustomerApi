32c247ea27e73877d7dfa09bede9f1376902821a
Phongsatorn <105865573+NewtronicZ@users.noreply.github.com>
2024-03-03 13:14:55 +0700

    Delete backend directory

--- backend/config/mongodb.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    //url: 'mongodb://127.0.0.1:27017/customercrud'
-
-    url: 'mongodb+srv://newdevilgods:Pnew2578205@cluster0.nogm3hl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-}
